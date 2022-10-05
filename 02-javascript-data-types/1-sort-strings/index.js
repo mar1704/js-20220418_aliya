8aac4977ac8302859d89076539bd393606a750a7
Aliya <bugaga789@yandex.ru>
2022-10-05 14:51:47 +0300

    2.1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -6,4 +6,18 @@
  */
 export function sortStrings(arr, param = 'asc') {
 
+    const directions = {
+        asc: 1,
+        desc: -1
+      };
+      const direction = directions[param];
+    
+      return makeSorting(arr, direction);
+    }
+    
+    function makeSorting(array, direction) {
+      return [...array].sort((string1, string2) =>
+        direction * string1.localeCompare(string2, ['ru', 'en'], {caseFirst: 'upper'}));
+
+    
 }
